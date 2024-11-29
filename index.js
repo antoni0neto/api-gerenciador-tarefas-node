3101e9ad8935b49c9751e6dbf7501a56c20b460e
antoni0neto <antonio.silva6@yahoo.com.br>
2024-11-29 11:24:51 -0300

    feat: add route to create a new task and put task and delete task

--- index.js
+++ index.js
@@ -2,15 +2,50 @@ const express = require("express");
 const dotenv = require("dotenv");
 
 const connectToDatabase = require("./src/database/mongoose.database");
+const TaskModel = require("./src/models/task.model");
 
 dotenv.config();
 const app = express();
+app.use(express.json());
 
 connectToDatabase();
 
-app.get("/tasks", (req, res) => {
-    const tasks = [{ id: 1, title: "Task 1" }];
-    res.status(200).send("Hello World!");
+app.get("/tasks", async (req, res) => {
+    try {
+        const tasks = await TaskModel.find();
+        res.status(200).send(tasks);
+    } catch (error) {
+        res.status(500).send(error.message);
+    }
+});
+
+app.post("/tasks", async (req, res) => {
+    try {
+        const task = await TaskModel.create(req.body);
+        res.status(201).send(task);
+    } catch (error) {
+        res.status(500).send(error.message);
+    }
+});
+
+app.put("/tasks/:id", async (req, res) => {
+    try {
+        const { id } = req.params;
+        const task = await TaskModel.findByIdAndUpdate(id, req.body);
+        res.status(200).send(task);
+    } catch (error) {
+        res.status(500).send(error.message);
+    }
+});
+
+app.delete("/tasks/:id", async (req, res) => {
+    try {
+        const { id } = req.params;
+        const task = await TaskModel.findByIdAndDelete(id);
+        res.status(200).send(task);
+    } catch (error) {
+        res.status(500).send(error.message);
+    }
 });
 
 app.listen(8000, console.log("Listening on port 8000"));
